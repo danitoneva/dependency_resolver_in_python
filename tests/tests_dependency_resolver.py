d2873ca0c2cc1e9078b465b3a1da459960aea605
danitoneva <d.toneva2003@gmail.com>
2026-06-15 11:11:02 +0300

    tests for the file dependency resolver

--- tests/tests_dependency_resolver.py
+++ tests/tests_dependency_resolver.py
@@ -1,16 +1,54 @@
+"""Tests for dependency resolver."""
 import pytest
 from src.dependency_resolver import DependencyResolver
 
 
 class TestDependencyResolver():
-    def test_dependency_resolver_successful(self):
-        assert DependencyResolver.resolve("A") == ["E", "G", "D", "F", "B", "A"]
+    """"This class checks if the packages are installed in the correct order."""
+    def test_dependency_resolver_A(self):
+        expected = ["E", "G", "D", "F", "B", "A"]
+        result = DependencyResolver.resolve("A")
+        assert expected == result
 
-    def test_dependency_resolver_no_dependency(self):
-        assert DependencyResolver.resolve("E") == ["E"]
+    def test_dependency_resolver_B(self):
+        expected = ["F", "B"]
+        result = DependencyResolver.resolve("B")
+        assert expected == result
+    
+    def test_dependency_resolver_C(self):
+        expected = ["F", "B", "E", "G", "D", "F", "B", "G", "C"]
+        result = DependencyResolver.resolve("C")
+        assert expected == result
 
-    def test_dependency_resolver_empty(self):
-        assert DependencyResolver.resolve()
+    def test_dependency_resolver_D(self):
+        expected = ["G", "D"]
+        result = DependencyResolver.resolve("D")
+        assert expected == result
+
+    def test_dependency_resolver_E(self):
+        expected = ["E"]
+        result = DependencyResolver.resolve("E")
+        assert expected == result
+
+    def test_dependency_resolver_F(self):
+        expected = ["F"]
+        result = DependencyResolver.resolve("F")
+        assert expected == result
+
+    def test_dependency_resolver_G(self):
+        expected = ["G"]
+        result = DependencyResolver.resolve("G")
+        assert expected == result
+
+    def test_dependency_resolver_H(self):
+        expected = ["F", "B", "E", "G", "D", "F", "B", "A", "H"]
+        result = DependencyResolver.resolve("H")
+        assert expected == result
+
+    def test_dependency_resolver_I(self):
+        expected = ["F", "B", "E", "G", "D", "F", "B", "A", "H", "I"]
+        result = DependencyResolver.resolve("I")
+        assert expected == result
 
 if __name__ == '__main__':
    pytest.main()
